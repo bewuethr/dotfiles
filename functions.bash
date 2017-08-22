392db60b49378313c361ba3cafd4d1eb836e02a7
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2017-08-22 00:50:05 -0400

    Add functions.bash

---
+++ functions.bash
@@ -0,0 +1,10 @@
+# Find lexically last directory that starts with provided parameter and cd into it
+cdl () {
+    local cands=("$1"*)
+    cd "${cands[-1]}"
+}
+
+# Fold text file at spaces to 80 columns
+wrap () {
+    fold --spaces "$1" > "$1".$$ && mv "$1".$$ "$1"
+}
