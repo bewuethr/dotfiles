84557d5ca4d7e35bf0ac96b20d4d7b48e18fefc3
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-07-31 16:35:13 -0400

    Add percent encoding filter

--- functions.bash
+++ functions.bash
@@ -10,6 +10,23 @@ wrap () {
 	fold --spaces "$1" > "$1".$$ && mv "$1".$$ "$1"
 }
 
+# Filter for percent encoding - encodes space as "%20" and not as "+"
+percentencode () {
+	local line i res
+	local re='[]:/?#@!$&'"'"'()*+,;=% []'
+	while IFS= read -r line; do
+		for (( i = 0; i < ${#line}; ++i )); do
+			local l=${line:i:1}
+			if [[ $l =~ $re ]]; then
+				res+=$(printf '%%%02X' "'$l")
+			else
+				res+=$l
+			fi
+		done
+		printf '%s\n' "$res"
+	done < "${1:-/dev/stdin}"
+}
+
 # Local function definitions
 if [[ -f $HOME/.functions_local.bash ]]; then
 	# shellcheck source=/dev/null
