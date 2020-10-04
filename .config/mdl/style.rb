626545262cc3fd0800a1dfbabd7445b9a6124a32
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2020-10-03 20:15:12 -0400

    Update mdl config

--- .config/mdl/style.rb
+++ .config/mdl/style.rb
@@ -1,7 +1,19 @@
 all
 
-# Don't enforce line length in code blocks
-rule 'MD013', :code_blocks => false
+# Use MD041 instead
+exclude_rule 'MD002'
+
+# Require ATX-style headers
+rule 'MD003', :style => :atx
+
+# Require dashes for unordered lists
+rule 'MD004', :style => :dash
+
+# Allow exactly 2 breaking spaces at the end of a line
+rule 'MD009', :br_spaces => 2
+
+# Don't enforce line length in code blocks and tables
+rule 'MD013', :code_blocks => false, :tables => false
 
 # Allow duplicate titles
 exclude_rule 'MD024'
@@ -15,5 +27,5 @@ rule 'MD029', :style => :ordered
 # Allow inline HTML
 exclude_rule 'MD033'
 
-# Don't require a language on fenced codeblocks
-exclude_rule 'MD040'
+# Require "---" for horizontal rules
+rule 'MD035', :style => '---'
