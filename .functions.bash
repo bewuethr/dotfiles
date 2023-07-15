d4b3ae1ef14c94d96c829b93f94476be1e43fcf2
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2023-07-14 21:40:00 -0400

    Shorten bundler version output

--- .functions.bash
+++ .functions.bash
@@ -82,9 +82,9 @@ upgradegems() (
 	gv="$gv" yq '.jobs.build.steps[1].with.rubygems |= env(gv)' "$wf" \
 		| diff --ignore-all-space --ignore-blank-lines "$wf" - \
 		| patch "$wf" -
-	git diff
-	git add "$wf" Gemfile.lock
-	git commit --message "Bump gems to $gv and bundler to $bv"
+	echo git diff
+	echo git add "$wf" Gemfile.lock
+	echo git commit --message "Bump gems to $gv and bundler to ${bv#Bundler version }"
 )
 
 # Install latest version of golangci-lint
