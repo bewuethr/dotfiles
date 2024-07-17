278170a5ec14450aa3658a76cf9b7a4b646a5ccb
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2024-07-17 12:27:03 -0400

    Add vim-fugitive to nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -790,6 +790,7 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
+  'tpope/vim-fugitive', -- Vim Git plugin
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
