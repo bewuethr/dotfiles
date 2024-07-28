344d7cc6d9565619f0e0a4c3892df918f689444c
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2024-07-28 12:48:50 -0400

    Remove hlsearch = true, as it's now default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,8 +71,7 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
+-- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
