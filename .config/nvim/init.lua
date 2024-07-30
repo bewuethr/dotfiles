a36bcb836beeee9c86a2e0d92a597dce5f0635d1
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2024-07-30 11:57:46 -0700

    Move lazydev and luvit-meta as LSP dependencies
    This enables lazy-loading; the plugins will always load when
    nvim-lspconfig loads.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -305,7 +305,25 @@ require('lazy').setup({
     end,
   },
 
-  { -- LSP Configuration & Plugins
+  -- LSP plugins
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim APIs
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the "vim.uv" word is found
+        {
+          path = 'luvit-meta/library',
+          words = { 'vim%.uv' },
+        },
+      },
+    },
+  },
+  { 'Bilal2453/luvit-meta', lazy = true },
+  {
+    -- Main LSP configuration
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
@@ -316,26 +334,6 @@ require('lazy').setup({
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Allows extra capabilities provided by nvim-cmp
-      'hrsh7th/cmp-nvim-lsp',
-
-      -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-      -- used for completion, annotations and signatures of Neovim apis
-      {
-        'folke/lazydev.nvim',
-        ft = 'lua',
-        opts = {
-          library = {
-            -- Load luvit types when the "vim.uv" word is found
-            {
-              path = 'luvit-meta/library',
-              words = { 'vim%.uv' },
-            },
-          },
-        },
-      },
-      { 'Bilal2453/luvit-meta', lazy = true },
     },
     config = function()
       -- Brief aside: **What is LSP?**
