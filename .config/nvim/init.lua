86bdc97c51703e66cab43b17e897b6d5bd793a6b
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2024-07-30 11:38:05 -0700

    Defer setting the clipboard syncing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,9 +25,12 @@ vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
+-- Schedule the setting after `UiEnter` because it can increase startup time.
+-- Remove this option if you want your OS clipboard to remain independent.
+-- See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
 
 -- Enable break indent
 vim.opt.breakindent = true
