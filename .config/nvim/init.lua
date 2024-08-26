9e9ab9b172e7092a2bdb0828eb902b435305bf7d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2024-08-26 15:55:16 -0700

    Add visual mode support for LSP code action keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -187,7 +187,7 @@ require('lazy').setup({
         mappings = vim.g.have_nerd_font,
         keys = {},
         spec = { -- Document existing key chains
-          { '<leader>c', group = '[C]ode' },
+          { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
           { '<leader>d', group = '[D]ocument' },
           { '<leader>r', group = '[R]ename' },
           { '<leader>s', group = '[S]earch' },
@@ -379,8 +379,9 @@ require('lazy').setup({
           --
           -- In this case, we create a function that lets us more easily define mappings specific
           -- for LSP related items. It sets the mode, buffer and description for us each time.
-          local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
           -- Jump to the definition of the word under your cursor.
@@ -414,7 +415,7 @@ require('lazy').setup({
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
