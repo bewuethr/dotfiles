08edc99143670427f735cbad725f9de81f8f445d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2024-08-28 13:18:04 -0700

    Add Actions fileytype and set actionlint as linter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -102,6 +102,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Add Actions-specific filetype
+vim.filetype.add {
+  pattern = { ['.*/.github/workflows/.*%.ya?ml'] = 'yaml.ghaction' },
+}
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
