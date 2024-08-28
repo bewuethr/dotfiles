974416716aef3fff73dd74189d3fdc5376231a6a
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2024-08-27 18:50:23 -0700

    Add standardrb as Ruby formatter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -580,6 +580,7 @@ require('lazy').setup({
       formatters_by_ft = {
         javascript = { 'prettier' },
         lua = { 'stylua' },
+        ruby = { 'standardrb' },
         sh = { 'shfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
