80444d22e69e7342e30af6dd74c1aecb3a43f85e
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2024-09-04 13:26:04 -0700

    Enable gopls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -488,10 +488,10 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         bashls = {},
+        gopls = {},
         ruby_lsp = {},
         tsserver = {},
         -- clangd = {},
-        -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
