d6235a21373940595a5c5b2133d189ec7470a802
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2024-09-09 08:20:56 -0700

    Update Neovim plugins, rename tsserver to ts_ls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -490,19 +490,7 @@ require('lazy').setup({
         bashls = {},
         gopls = {},
         ruby_lsp = {},
-        tsserver = {},
-        -- clangd = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        ts_ls = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
