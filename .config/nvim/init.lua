4a1301751d50007726a0ac89916ab734c1e8b630
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2025-03-06 14:33:12 -0800

    Add autocmd to update Go imports on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -128,6 +128,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Format Go file and update imports when saving
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { 'source.organizeImports' } }
+    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, 1000)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format { async = false }
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
