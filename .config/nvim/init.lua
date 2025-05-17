06bf726d763379fabfc2e545ec0eea61fac9acf3
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2025-05-17 13:59:10 -0700

    Remove redundant diagnostic block

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -489,15 +489,6 @@ require('lazy').setup {
         virtual_text = {
           source = 'if_many',
           spacing = 2,
-          format = function(diagnostic)
-            local diagnostic_message = {
-              [vim.diagnostic.severity.ERROR] = diagnostic.message,
-              [vim.diagnostic.severity.WARN] = diagnostic.message,
-              [vim.diagnostic.severity.INFO] = diagnostic.message,
-              [vim.diagnostic.severity.HINT] = diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
-          end,
         },
         virtual_lines = true,
       }
