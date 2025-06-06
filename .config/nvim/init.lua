0c9209eb539853b596cc7e968358a7b1379fa2cb
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2025-06-06 16:18:17 -0400

    Fix function name for highlighting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -130,7 +130,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.hl.on_yank()
+    vim.highlight.on_yank()
   end,
 })
 
