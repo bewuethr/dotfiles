a305d56b96fa837a26682f54146c97f8f2c9881b
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2025-06-27 12:54:03 -0700

    Enable spellcheck for Markdown files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -153,6 +153,15 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
+-- Enable spellcheck for markdown files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_ca'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
