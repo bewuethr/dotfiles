9bd3f95d8a388da77d5f5a97b782b6a64f5817c3
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2026-01-27 09:58:49 -0800

    Update with changes from upstream

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -246,7 +246,7 @@ require('lazy').setup {
         mappings = true, -- enable mapping icons
         keys = {},
         spec = { -- Document existing key chains
-          { '<leader>s', group = '[S]earch' },
+          { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
           { '<leader>t', group = '[T]oggle' },
           { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
           { 'gr', group = 'LSP Actions', mode = { 'n' } },
@@ -332,11 +332,12 @@ require('lazy').setup {
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
@@ -632,6 +633,9 @@ require('lazy').setup {
         end
       end,
       formatters = {
+        buildifier = {
+          prepend_args = { '-buildifier_disable', 'loadTop' },
+        },
         ledger_fmt = {
           command = 'ledger_fmt',
           args = '$FILENAME',
