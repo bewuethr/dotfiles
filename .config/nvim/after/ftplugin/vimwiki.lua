d343d6b568d225374b2c349cd741b2dd201da73d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2026-04-13 12:27:50 -0700

    Add vimwiki "after" styling

---
+++ .config/nvim/after/ftplugin/vimwiki.lua
@@ -0,0 +1,9 @@
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.expandtab = true
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+vim.opt_local.breakindentopt = 'shift:2,sbr'
+vim.opt_local.showbreak = '> '
